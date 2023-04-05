949c2aa5809897f860900941fb52a7469e1abac1
hyperGT <charlesgabriel102@gmail.com>
2023-04-05 13:06:52 -0300

    Arco-Iris - Exercicio Set #1 - para fazer

---
+++ src/Collections/Set/Exercicios/Arco_Iris.java
@@ -0,0 +1,28 @@
+package Collections.Set.Exercicios;
+
+public class Arco_Iris {
+
+    //cria um conjunto contendo as cores do arco-iris
+
+
+
+    //exiba a quantidade de cores do arco-iris tem
+
+
+    //exiba as cores em ordem alfabetica(a-z)
+
+
+    //exiba na ordem inversa da que foi informada(z-a)
+
+
+    //exiba todas que começam com v
+
+
+    //remova todas que não começam com a letra v
+
+
+    //limpe o conjunto
+
+
+    //confira se o conjunto está vazio
+}
